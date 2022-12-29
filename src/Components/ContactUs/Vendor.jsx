b9ea779fb3f0227ca40d9d8d0b6f1adbc5d81285
Yugal Khanter <yugalkhanter82@gmail.com>
2022-12-30 01:19:19 +0530

    vendors form and career forms functionality added

--- src/Components/ContactUs/Vendor.jsx
+++ src/Components/ContactUs/Vendor.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Vendor = () => {
+
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: undefined,
+        address: '',
+        material: ''
+    });
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const data = formData;
+        console.log(data);
+        clearFormData();
+        document.getElementById('enquire-form').reset()
+    }
+
+    const handleFirstNameInputChange = (e) => {
+        const value = e.target.value;
+        setFormData({ ...formData, firstName: value });
+    }
+
+    const handleLastNameInputChange = (e) => {
+        const value = e.target.value;
+        setFormData({ ...formData, lastName: value });
+    }
+
+    const handleEmailInputChange = (e) => {
+        const value = e.target.value;
+        setFormData({ ...formData, email: value });
+    }
+
+    const handlePhoneInputChange = (e) => {
+        const value = e.target.value;
+        setFormData({ ...formData, phone: value });
+    }
+
+    const handleAddressInputChange = (e) => {
+        const value = e.target.value;
+        setFormData({ ...formData, address: value });
+    }
+
+    const handleMaterialInputChange = (e) => {
+        const value = e.target.value;
+        setFormData({ ...formData, material: value });
+    }
+
+    const clearFormData = () => {
+        setFormData({
+            firstName: '',
+            lastName: '',
+            email: '',
+            phone: undefined,
+            address: '',
+            material: ''
+        });
+    }
     return (
         <div className='mt-10'>
-            <form className='w-1/2 mx-auto'>
+            <form onSubmit={handleFormSubmit} className='w-1/2 mx-auto'>
                 <div className="grid md:grid-cols-2 md:gap-6">
                     <div className="relative z-0 mb-6 w-full group">
                         <input
@@ -12,6 +70,8 @@ const Vendor = () => {
                             id="floating_first_name"
                             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                             placeholder=" "
+                            value={formData.firstName}
+                            onChange={handleFirstNameInputChange}
                         />
                         <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
                     </div>
@@ -22,6 +82,8 @@ const Vendor = () => {
                             id="floating_last_name"
                             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                             placeholder=" "
+                            value={formData.lastName}
+                            onChange={handleLastNameInputChange}
                         />
                         <label htmlFor="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
                     </div>
@@ -33,6 +95,8 @@ const Vendor = () => {
                         id="floating_email"
                         className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                         placeholder=" "
+                        value={formData.email}
+                        onChange={handleEmailInputChange}
                     />
                     <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
                 </div>
@@ -44,6 +108,8 @@ const Vendor = () => {
                             name="floating_phone" id="floating_phone"
                             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                             placeholder=" "
+                            value={formData.phone}
+                            onChange={handlePhoneInputChange}
                         />
                         <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number</label>
                     </div>
@@ -54,13 +120,22 @@ const Vendor = () => {
                             id="floating_phone"
                             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                             placeholder=" "
+                            value={formData.address}
+                            onChange={handleAddressInputChange}
                         />
                         <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Address</label>
                     </div>
                 </div>
                 <div className="grid md:grid-cols-1 md:gap-6">
                     <div className="relative z-0 mb-6 w-full group">
-                        <textarea rows='3' name="description" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <textarea
+                            rows='3'
+                            name="description" id="floating_phone"
+                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                            placeholder=" "
+                            value={formData.material}
+                            onChange={handleMaterialInputChange}
+                        />
                         <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Material (Descriptive)</label>
                     </div>
                 </div>
@@ -71,6 +146,7 @@ const Vendor = () => {
                     Submit
                 </button>
                 <button
+                    onClick={clearFormData}
                     type="reset"
                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-10 py-3 ml-4 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
